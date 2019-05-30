183ea97901931f2163ba6829a29bdf15b3a60512
nashaque123 <nashaque123@gmail.com>
2019-05-30 20:46:16 +0100

    Game Event System Finished
    Can now add listeners to game events meaning the modularity of the project has improved

---
+++ Assets/Scripts/HealthSystem/WrestlerHealth.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class WrestlerHealth : MonoBehaviour
+{
+    public UnityEvent TakeDamage;
+    // public GlobalInteger Health;
+    int counter = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counter <= 10)
+        {
+            counter++;
+        }
+        else
+        {
+            TakeDamage.Invoke();
+        }
+    }
+}
